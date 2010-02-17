0b68e5a30822005a8d6105022371769331a6bea4
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-02-17 21:09:07 +0000

    disparition de confse3.ini et utilisation /etc/se3/config_m.cache.sh par defaut

--- usr/share/se3/shares/shares.avail/no-gpo-upload.sh
+++ usr/share/se3/shares/shares.avail/no-gpo-upload.sh
@@ -8,11 +8,15 @@
 [ ! -f /home/netlogon/machine/$2/no-gpo-upload.lck ] && exit 0
 [ "$1" == "adminse3" ] && exit 0 # CPAU.exe
 
-source /var/se3/Progs/install/installdll/confse3.ini
-adminse3="$(echo $compte_admin_local|sed -e 's/\r//g')"
-passadmin="$(echo $password_admin_local|sed -e 's/\r//g')"
-se3ip="$(echo $ip_se3|sed -e 's/\r//g')"
-dom="$(echo $domaine|sed -e 's/\r//g')"
+if [ -e /etc/se3/config_m.cache.sh ]; then
+	. /etc/se3/config_m.cache.sh
+else
+	source /var/se3/Progs/install/installdll/confse3.ini
+	se3ip="$(echo $ip_se3|sed -e 's/\r//g')"
+	se3-domain="$(echo $domaine|sed -e 's/\r//g')"
+fi
+adminse3="adminse3"
+
 
 /usr/share/se3/logonpy/logon.py $1 $2 $4
 
@@ -32,6 +36,6 @@ else
 	SHARECMD="net share C\$=C:"
 fi
 
-sed -e s/%se3ip%/"$se3ip"/g /home/netlogon/EnableGPO.bat|sed -e s/%machine%/"$2"/g|sed -e s_%sharecmd%_"$SHARECMD"_g|sed s/%user%/$1/g|sed -e s/%domain%/$dom/g|sed -e s/netsh.*//g|sed -e s/.*printers.*//g > /home/netlogon/machine/$2/EnableGPO.bat
+sed -e s/%se3ip%/"$se3ip"/g /home/netlogon/EnableGPO.bat|sed -e s/%machine%/"$2"/g|sed -e s_%sharecmd%_"$SHARECMD"_g|sed s/%user%/$1/g|sed -e s/%domain%/$se3_domain/g|sed -e s/netsh.*//g|sed -e s/.*printers.*//g > /home/netlogon/machine/$2/EnableGPO.bat
 echo '\\'"$se3ip"'\netlogon\cpau.exe' -wait -lwop -hide -dec -file '\\'"$se3ip"'\netlogon\machine\gpo_helper.job' > /home/netlogon/machine/$2/fallback.bat
 echo '\\'"$se3ip"'\netlogon\machine\\'"$2\logon.bat" >>/home/netlogon/machine/$2/fallback.bat
