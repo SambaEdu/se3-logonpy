49e61f41a239a491f673b55e7fe83af70441b4a4
denis-b <denis.bonnenfant@diderot.org>
2016-09-14 14:32:56 +0200

    Desactivation de windows update
    On desactive totalement les mises a jour via windows update, pour éviter la saturation totale des postes ou du réseau.
    Ceci permet à wsusoffline de fonctionner correctement et sans surcharge !

--- usr/share/se3/logonpy/se3GPO.py
+++ usr/share/se3/logonpy/se3GPO.py
@@ -103,7 +103,8 @@ default_keys = [('HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Po
 		('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Gwx\DisableGwx', 'REG_DWORD', 'Vista,Seven', '1'),
 		('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\OSUpgrade\ReservationsAllowed', 'REG_DWORD', 'Vista,Seven', '0'),
 		('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\DisableOSUpgrade', 'REG_DWORD', 'Vista,Seven', '1'),
-		('HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\IncludeRecommendedUpdates', 'REG_DWORD', 'Vista,Seven', '0')]
+		('HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\IncludeRecommendedUpdates', 'REG_DWORD', 'Vista,Seven', '0'),
+		('HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAutoUpdate', 'REG_DWORD', 'Vista,Seven', '1')]
 
 #TODO Remove all style keys, it's now ntuser.dat default!
 firewall_keys = [('HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile\DoNotAllowExceptions', 'REG_DWORD', 'XP,Vista,Seven', '0'),
